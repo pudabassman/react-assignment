09cdc08ecd5f845db04e976518f8ee8720e99d38
Ami Parkhomovsky <ami.parkhomovsky@go-arc.com>
2022-02-27 00:06:27 +0200

    Removed toast

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import MessageForm from './MessageForm';
 import MeassageFeed from './MeassageFeed';
-import { Container, Toast, ToastContainer } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import React, { createRef } from 'react';
 import '../CSS/components/App.css'
 class App extends React.Component{
@@ -11,23 +11,17 @@ class App extends React.Component{
       toastBody: ""
     }
     this.messageAddedRef = createRef();
-    this.toggleToast = this.toggleToast.bind(this)
     this.onMessageAdded = this.onMessageAdded.bind(this)
   }
 
-  toggleToast(){
-    this.setState({showToast: !this.state.showToast})
-  }
-
   onMessageAdded(message){
-    this.messageAddedRef.current(message)
-    this.toggleToast(message)    
+    this.messageAddedRef.current(message)    
   }
 
   render() {
     return (
       <Container className="app rounded-3 px-0 bg-light">
-        <MessageForm onMessageAdded={this.onMessageAdded} toggleToast={this.toggleToast}/>
+        <MessageForm onMessageAdded={this.onMessageAdded}/>
         <MeassageFeed messageAdded={this.messageAddedRef}/>        
       </Container>
     );
